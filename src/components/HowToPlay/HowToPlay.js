531ebd81e02442c688b933faed0975a9ffeaf15c
ARoibu <aroibu@uwo.ca>
2021-08-13 12:26:28 -0400

    finished styling component, needs a couple tweaks

--- src/components/HowToPlay/HowToPlay.js
+++ src/components/HowToPlay/HowToPlay.js
@@ -3,28 +3,28 @@ import Fries from '../../images/french-fries-active.png'
 
 export default function HowToPlay() {
     return(
-        <section>
-            <h1>Collect points and get rewarded</h1>
-            <article>
-                <h2>How to play</h2>
-                <div>
-                    <img src={Fries} alt='Fries icon'/>
-                    <p>Place an order on the app</p>
+        <section className='howto'>
+            <h1 className='howto__header'>Collect points and get rewarded</h1>
+            <article className='howto__card'>
+                <h2 className='howto__subheader'>How to play</h2>
+                <div className='howto__step'>
+                    <img className='howto__fries' src={Fries} alt='Fries icon'/>
+                    <p className='howto__step--text'>Place an order on the app</p>
                 </div>
-                <div>
-                    <img src={Fries} alt='Fries icon'/>
-                    <p>Take a pic, upload to Twitter and tag your bestie</p>
+                <div className='howto__step'>
+                    <img className='howto__fries' src={Fries} alt='Fries icon'/>
+                    <p className='howto__step--text'>Take a pic, upload to Twitter and tag your bestie</p>
                 </div>
-                <div>
-                    <img src={Fries} alt='Fries icon'/>
-                    <p>Earn free rewards with every entry</p>
+                <div className='howto__step'>
+                    <img className='howto__fries' src={Fries} alt='Fries icon'/>
+                    <p className='howto__step--text'>Earn free rewards with every entry</p>
                 </div>
-                <input type='button' value='Play now'/>
+                <input className='howto__button' type='button' value='Play now'/>
             </article>
-            <p>Official Rules | Privacy Policy<br/>
+            <p className='howto__details'>Official Rules | Privacy Policy<br/>
             McDonalds Terms of Use | FAQ</p>
-            <p>*To enter without making a purchase, <a href=''>click here</a></p>
-            <p>LEGAL RESIDENTS OF CANADA, 18 OR OLDER AND WHO ARE MEMBERS OF THE
+            <p className='howto__details'>*To enter without making a purchase, <a href=''>click here</a></p>
+            <p className='howto__details'>LEGAL RESIDENTS OF CANADA, 18 OR OLDER AND WHO ARE MEMBERS OF THE
                 MCDONALDS CANADA LOYALITY PROGRAM AT THE TIME OF ENTRY. Participating stores only.
                 Promotion ends 8/31/21. For official rules and how to enter visit</p>
         </section>
